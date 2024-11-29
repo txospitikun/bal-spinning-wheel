b35debdfbe5973f2a78ead24b40026497472c74a
chinezprost <playschan@gmail.com>
2024-11-29 17:21:35 +0200

    1

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,20 @@
 const { randomUUID } = require("crypto");
 const express = require("express");
 const http = require("http");
+const cors = require("cors");
 const { Server } = require("socket.io");
 const fs = require('fs').promises;
 
 const app = express();
-const server = http.createServer(app);
-app.use(cors());
-const io = new Server({
+app.use(cors()); 
+
+const server = require("http").createServer(app);
+
+const io = new Server(server, {
   cors: {
-    origin: ["http://93.115.53.18:3000", "http://93.115.53.18:3000/roata", "http://localhost:3000", "http://localhost:3000/roata"],
-    methods: ["GET", "POST"],        
-  }
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
 
 io.listen(4000);
@@ -178,7 +181,7 @@ main = async () =>
 
   runStages()
 
-  const PORT = 3000;
+  const PORT = 3001;
   server.listen(PORT, () =>
   {
     console.log(`Server is running on http://93.115.53.18:${PORT}`);
